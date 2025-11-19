89b7ebac86e4dcb52a206d9c6f17914b1d92d7d8
S-iriogbe1 <sean.iriogbe@pau.edu.ng>
2025-11-19 14:02:07 +0100

    My week-8 commit, ill commit better next time

---
+++ week-8/practice_11/target/debug/practice_11.d
@@ -0,0 +1,1 @@
+C:\Users\user\Documents\s.iriogbeCOS101\week-8\practice_11\target\debug\practice_11.exe: C:\Users\user\Documents\s.iriogbeCOS101\week-8\practice_11\src\main.rs
---
+++ week-8/practice_2/target/debug/practice_2.d
@@ -0,0 +1,1 @@
+C:\Users\user\Documents\s.iriogbeCOS101\week-8\practice_2\target\debug\practice_2.exe: C:\Users\user\Documents\s.iriogbeCOS101\week-8\practice_2\src\main.rs
