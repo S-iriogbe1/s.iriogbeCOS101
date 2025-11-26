78bd416fb8d55ac2a4f98c55e39594e5860b7cd3
S-iriogbe1 <sean.iriogbe@pau.edu.ng>
2025-11-26 12:45:00 +0100

    Append data to a file

--- week-9/practice_2/src/main.rs
+++ week-9/practice_2/src/main.rs
@@ -1,6 +1,7 @@
 use std::io::Read;
 
 fn main() {
+	let mut a_file = std::fs::File::create("data.txt").expect("create failed");
 	let mut file = std::fs::File::open("welcome_message.txt").unwrap();
 	let mut contents = String::new();
 	file.read_to_string(&mut contents).unwrap();
